3dc60bed1160278ebb531b329976f27e9afff6c1
xenacode-art <kaja.obinna@lmu.edu.ng>
2022-02-02 08:59:40 +0100

    Python project that detects covid 19 with AI Algorithm

---
+++ README.md
@@ -0,0 +1,13 @@
+# COVIDC: An Expert System to Diagnose COVID-19 and Predict its Severity using Chest CT Scans: Application in Radiology
+## Dependencies
+1- python 3.6 
+2- Scikit Learn 0.23 
+3- Tensorflow 
+4- DenseNet121 CNN based pretrained model
+
+## How to use?
+Please see covidc_with_weights.py (For its webserver please visit https://covidc.pythonanywhere.com/)
+## Citation details
+Wajid Arshad Abbasi, Syed Ali Abbas, Saiqa Andleeb, Ghafoor ul Islam, Syeda Adin Ajaz, Kinza Arshad, Sadia Khalil, Asma Anjam, Kashif Ilyas, Mohsib Saleem, Jawad Chughtai, Ayesha Abbas,
+COVIDC: An Expert System to Diagnose COVID-19 and Predict its Severity using Chest CT Scans: Application in Radiology,
+Informatics in Medicine Unlocked, 2021,100540, https://doi.org/10.1016/j.imu.2021.100540.
---
+++ covidc_with_weights.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  5 17:03:06 2020
+
+@author: Dr_Wajid (wajidarshad@gmail.com)
+"""
+
+import numpy as np
+from sklearn import preprocessing
+from tensorflow.keras.applications.densenet import DenseNet121
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.densenet import preprocess_input
+
+def extract_transfer_learning_features(img_path):
+    model = DenseNet121(weights='imagenet', include_top=False)
+    img = image.load_img(img_path, target_size=(331, 331))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    features = model.predict(x).flatten()
+    return features
+def standarized_normalized_severity(features, norm='yes'):
+    features=(features-np.load('trained_models/mean_covid_vs_non.npy'))/(np.load('trained_models/std_covid_vs_non.npy')+0.0001)
+    #features=preprocessing.scale(features)
+    if norm=='yes':
+        features=preprocessing.normalize(features)
+    return features
+def standarized_normalized_c_vs_n(features, norm='yes'):
+    features=(features-np.load('trained_models/mean_covid_vs_non.npy'))/(np.load('trained_models/std_covid_vs_non.npy')+0.0001)
+    #features=preprocessing.scale(features)
+    if norm=='yes':
+        features=preprocessing.normalize(features)
+    return features
+def apply_covidc(image_path):
+    feats=extract_transfer_learning_features(image_path)
+    trained_model_c_vs_n_w=np.load('trained_models/weights/weight_vector_SVM_densent_covid_vs_noncovid.npy')
+    trained_model_c_vs_n_b=np.load('trained_models/weights/bias_SVM_densent_covid_vs_noncovid.npy')
+    
+    if np.dot(trained_model_c_vs_n_w[0],standarized_normalized_c_vs_n([feats])[0])+trained_model_c_vs_n_b[0]<=0:
+        print("Diagnosis: non-COVID")
+    else:
+        print('Diagnosis: COVID-19')
+        trained_model_severity_w=np.load('trained_models/weights/weight_vector_SVM_densent_covid_severity.npy')
+        trained_model_severity_b=np.load('trained_models/weights/bias_SVM_densent_covid_severity.npy')
+        if np.dot(trained_model_severity_w[0],standarized_normalized_severity([feats])[0])+trained_model_severity_b[0]<=0:
+            print("Severity: Mild")
+        else:
+            print("Severity: Severe")
+    
+if __name__ == "__main__":
+    apply_covidc('input_image_example.png')
